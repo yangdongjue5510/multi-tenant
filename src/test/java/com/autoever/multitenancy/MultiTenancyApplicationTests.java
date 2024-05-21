d3480c5d8bacb8735589e24e9201f85da11313b5
1128016 <djwhy5510@hyundai-autoever.com>
2024-05-21 14:29:51 +0900

    init project

---
+++ src/test/java/com/autoever/multitenancy/MultiTenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.multitenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
